44125d3e6d9cb7183c2c081d2cda379df5ad1856
Vinny King Michael <95449899+vinnyxKing@users.noreply.github.com>
2022-06-19 23:08:44 +0200

    version 3.1.5

--- lib/devices/device_info.dart
+++ lib/devices/device_info.dart
@@ -1,8 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:findeers_app/utilities/auth_method.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_scan_bluetooth/flutter_scan_bluetooth.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import '../maps/mapUtility.dart';
 import '../maps/maps.dart';
 import 'connect.dart';
@@ -17,7 +16,15 @@ class Body_detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
+    return Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/w.jpg"))),
+    child: SingleChildScrollView(
       child: Column(
         children: <Widget>[
           SizedBox(
@@ -43,12 +50,12 @@ class Body_detail extends StatelessWidget {
                       label: Text(
                         "Find device",
                         style: Theme.of(context).textTheme.headline5?.copyWith(
-                            color: Color.fromARGB(255, 190, 180, 180),
+                            color: Colors.white,
                             fontWeight: FontWeight.bold),
                       ),
                       style: TextButton.styleFrom(
                           primary: Colors.white,
-                          backgroundColor: Color.fromARGB(255, 25, 26, 25)),
+                          backgroundColor: Color(0xff0099DD)),
                       onPressed: () {
                         mapsspace space = mapsspace(devices.val_rssi);
 
@@ -60,12 +67,12 @@ class Body_detail extends StatelessWidget {
                       label: Text(
                         "Delete Device",
                         style: Theme.of(context).textTheme.headline5?.copyWith(
-                            color: Color.fromARGB(255, 190, 180, 180),
+                            color: Colors.black,
                             fontWeight: FontWeight.bold),
                       ),
                       style: TextButton.styleFrom(
                           primary: Colors.white,
-                          backgroundColor: Color.fromARGB(255, 25, 26, 25)),
+                          backgroundColor: Color(0xffBF1304)),
                       onPressed: () async {
                         AuthUser user = AuthUser();
                         String email = user.auth.currentUser!.email.toString();
@@ -90,7 +97,7 @@ class Body_detail extends StatelessWidget {
             ]),
           )
         ],
-      ),
+      ),)
     );
   }
 }
@@ -137,25 +144,32 @@ class DeviceTitleImageDisplay extends StatelessWidget {
         children: <Widget>[
           Text(
             "\n\nDevice Name",
-            style: TextStyle(color: Colors.white),
+            style: GoogleFonts.ubuntu(fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )
           ),
           Text(
             devices.name,
             style: Theme.of(context)
                 .textTheme
                 .headline4
-                ?.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+                ?.copyWith(color: Colors.black, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: kdefaultpaddin),
           Row(
             children: <Widget>[
               RichText(
                   text: TextSpan(children: [
-                TextSpan(text: "Mac_Address \n"),
+                TextSpan(text: "Mac_Address \n",
+                style: GoogleFonts.josefinSans(fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff025159),
+                ),),
                 TextSpan(
                     text: "\$${devices.address}\n",
                     style: Theme.of(context).textTheme.headline4?.copyWith(
-                        color: Colors.white, fontWeight: FontWeight.bold))
+                        color: Color(0xff012030), fontWeight: FontWeight.bold))
               ])),
               SizedBox(width: kdefaultpaddin),
               Expanded(
--- lib/maps/home_screen.dart
+++ lib/maps/home_screen.dart
@@ -43,55 +43,57 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/log4.jpg"),
-    fit: BoxFit.cover)),
-    child: Scaffold(
-backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: Text("Findeers",
-            textDirection: TextDirection.ltr,
-            style: GoogleFonts.lobster(fontSize: 40,
-            fontWeight: FontWeight.bold,
-              color: Colors.blueGrey,
-        ),),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.arrow_back),
-          color: Colors.black,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              SharedPreferences pref = await SharedPreferences.getInstance();
-              pref.remove('email');
-              AuthUser service = AuthUser();
-              service.logOutUser(context);
-            },
-            icon: Icon(Icons.logout),
-            color: Colors.black,
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/log4.jpg"), fit: BoxFit.cover)),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Text(
+              "Findeers",
+              textDirection: TextDirection.ltr,
+              style: GoogleFonts.lobster(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey,
+              ),
+            ),
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back),
+              color: Colors.black,
+            ),
+            actions: [
+              IconButton(
+                onPressed: () async {
+                  SharedPreferences pref =
+                      await SharedPreferences.getInstance();
+                  pref.remove('email');
+                  AuthUser service = AuthUser();
+                  service.logOutUser(context);
+                },
+                icon: Icon(Icons.logout),
+                color: Colors.black,
+              ),
+            ],
           ),
-        ],
-      ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: kdefaultpaddin),
+              ),
+              sections(),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kdefaultpaddin),
-
-          ),
-          sections(),
-
-          //  storeddevices(),
-          Expanded(
-              child: StreamBuilder(
+              //  storeddevices(),
+              Expanded(
+                  child: StreamBuilder(
                 //Fetching data from the documentId specified of the student
-                stream: users.doc(useremail).collection('Bluetooth').snapshots(),
+                stream:
+                    users.doc(useremail).collection('Bluetooth').snapshots(),
                 builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                   //Error Handling conditions
                   if (snapshot.hasError) {
@@ -103,7 +105,8 @@ backgroundColor: Colors.transparent,
                     return ListView.builder(
                       itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, index) {
-                        final DocumentSnapshot users = snapshot.data!.docs[index];
+                        final DocumentSnapshot users =
+                            snapshot.data!.docs[index];
                         String id = snapshot.data!.docs[index].id.toString();
                         upmac = users['mac'];
                         upname = users['name'];
@@ -112,8 +115,8 @@ backgroundColor: Colors.transparent,
 
                         devr.add(DisplayDevices(
                             address: upmac, name: upname, val_rssi: uprssi));
-
                         return Card(
+                          color: Color(0xffDEEFE7),
                           child: ListTile(
                             leading: Text(users['rssi']),
                             subtitle: Text(users['mac']),
@@ -123,9 +126,9 @@ backgroundColor: Colors.transparent,
                                   context,
                                   MaterialPageRoute(
                                       builder: (context) => DeviceInfo(
-                                        devices: devr[index],
-                                        delid: id,
-                                      )));
+                                            devices: devr[index],
+                                            delid: id,
+                                          )));
                             }
                             // Navigator.of(context).push(MaterialPageRoute(
                             // builder: (context) => HomeScreen(devr)));
@@ -141,15 +144,15 @@ backgroundColor: Colors.transparent,
                   }
                 },
               ))
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => ScanBlue()));
-        },
-        child: Icon(Icons.search),
-      )),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => ScanBlue()));
+            },
+            child: Icon(Icons.search),
+          )),
     );
   }
 }
